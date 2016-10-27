256f55c8a78d68492e082bb2d78c7728963f964c
giraldez122 <giraldez122@gmail.com>
2016-10-27 14:36:31 +0200

    Create helloworld.java
    Contains helloworld in 3 languages

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+
+  public static void main(String[]args){
+    System.out.println("Hello world");
+    System.out.println("Hola mundo");
+    System.out.println("Bos dias mundo");    
+  }
+}
